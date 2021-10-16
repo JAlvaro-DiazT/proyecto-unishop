6eeb00330a33561108a26d7c4cbb6d47947951ed
Alvaro Diaz <jadiazt_1@uqvirtual.edu.co>
2021-10-16 14:21:19 -0500

    Se agrego al .sql los registros de compra y categoria

--- persistencia/src/test/resources/datosUnishop.sql
+++ persistencia/src/test/resources/datosUnishop.sql
@@ -4,13 +4,30 @@ insert into Ciudad values (2,"Cali");
 insert into Ciudad values (3,"Medellin");
 insert into Ciudad values (4,"Barranquilla");
 
-
+--Registros de administrador
 insert into Administrador values (001,"Raul Montez","rmontez@hotmail.com","ramiro88**");
 insert into Administrador values (002,"Julian Hernandez","julih@gmail.com","rGtY66@g");
 insert into Administrador values (003,"Lucia Ramirez","lucir@mail.com","luci200##");
 insert into Administrador values (004,"Miriam Montero","m_montero13@hotmail.com","m12345");
 
+----Registros de usuario
 insert into Usuario values (100,"Jarib Romero","j_a_r260@gmail.com","123Jarib20@",1);
 insert into Usuario values (101,"Zulma Bedoya","bedoyita2020@gmail.com","zbedoya40",1);
 insert into Usuario values (102,"Armando Roa","roaarnando600@gmail.com","20TdUn",3);
 insert into Usuario values (100,"Miranda Aldana","58aldana@mail.com","57393G^nHt^",2);
+
+--Registros de telefonos de usuario
+
+
+--Registros de compra
+insert into Compra values(0001,2021/10/16,"Debito");
+insert into Compra values(0002,2021/10/16,"Credito");
+insert into Compra values(0003,2021/10/16,"Credito");
+insert into Compra values(0004,2021/10/16,"Debito");
+
+--Registros de categoria
+insert into Categoria values(01,"Electrodomesticos");
+insert into Categoria values(02,"Herramientas");
+insert into Categoria values(03,"Equipamiento medico");
+
+--
