30c1ad54c5b94c0519e3c1f0de4ceb738911e9e8
Carolina <ctorresc@uqvirtual.edu.co>
2021-10-16 04:10:20 -0500

    se agregaron datos sql de ciudad,administrador y usuarios para test

--- persistencia/src/test/resources/datosUnishop.sql
+++ persistencia/src/test/resources/datosUnishop.sql
@@ -1,2 +1,16 @@
 --Registros de ciudad
+insert into Ciudad values (1,"Bogota");
+insert into Ciudad values (2,"Cali");
+insert into Ciudad values (3,"Medellin");
+insert into Ciudad values (4,"Barranquilla");
 
+
+insert into Administrador values (001,"Raul Montez","rmontez@hotmail.com","ramiro88**");
+insert into Administrador values (002,"Julian Hernandez","julih@gmail.com","rGtY66@g");
+insert into Administrador values (003,"Lucia Ramirez","lucir@mail.com","luci200##");
+insert into Administrador values (004,"Miriam Montero","m_montero13@hotmail.com","m12345");
+
+insert into Usuario values (100,"Jarib Romero","j_a_r260@gmail.com","123Jarib20@",1);
+insert into Usuario values (101,"Zulma Bedoya","bedoyita2020@gmail.com","zbedoya40",1);
+insert into Usuario values (102,"Armando Roa","roaarnando600@gmail.com","20TdUn",3);
+insert into Usuario values (100,"Miranda Aldana","58aldana@mail.com","57393G^nHt^",2);
